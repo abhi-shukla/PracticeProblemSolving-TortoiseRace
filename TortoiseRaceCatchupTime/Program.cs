62ebd9a42c8389f84518b90685af4e02ccd49c89
Abhishek Shukla <abhishek.shukla-ext@jci.com>
2015-11-30 12:19:57 -0600

    Add code

---
+++ TortoiseRaceCatchupTime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TortoiseRaceCatchupTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = GetTimeToCatchUp(720, 850, 70);
+            Console.WriteLine("[ " + time[0] + ", " + time[1] + ", " + time[2] + " ]");
+
+            time = GetTimeToCatchUpUsingTimeSpan(80, 91, 37);
+            Console.WriteLine("[ " + time[0] + ", " + time[1] + ", " + time[2] + " ]");
+            Console.ReadLine();
+        }
+
+        private static int[] GetTimeToCatchUp(int v1, int v2, int g)
+        {
+            if (v1 >= v2) return null;
+            int seconds = (int)((double)g / (v2 - v1) * 3600);
+            return new int[] { seconds / 3600, seconds % 3600 / 60, seconds % 60 };
+        }
+
+        private static int[] GetTimeToCatchUpUsingTimeSpan(int v1, int v2, int g)
+        {
+            if (v1 >= v2) return null;
+            var ts = System.TimeSpan.FromSeconds((g * 3600) / (v2 - v1));
+            return new[] { ts.Hours, ts.Minutes, ts.Seconds };
+        }
+    }
+}
